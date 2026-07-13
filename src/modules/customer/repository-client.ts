9ff1894c3e1dc5838e7db68f9da35a1c7418b733
unknown <evertonjose421@gmail.com>
2026-07-13 17:01:43 -0300

    13072026

--- src/modules/customer/repository-client.ts
+++ src/modules/customer/repository-client.ts
@@ -1,21 +1,32 @@
 import dbConn, { MOBILE, PUBLICO } from "../../database/connection/database-connection.ts";
 import { type  cad_clie } from "./contracts/cad_clie.ts";
 
-export async function getAllClients (codigo?:number){
+export async function getAllClients (codigo?:number, ativo?:'S' | 'N'){
 
-         let sql = ` select *,
+                         let baseSql = ` select *,
                                                 DATE_FORMAT(DATA_CADASTRO, '%Y-%m-%d') AS DATA_CADASTRO,
                                                 DATE_FORMAT(DATA_RECAD, '%Y-%m-%d %H:%i:%s') AS DATA_RECAD 
                                                 from ${PUBLICO}.cad_clie c
-                                                WHERE
-                                                 
                                                 `  
-                                                let complemnt = ` c.ativo = 'S';`;
-                                                if(codigo && codigo != undefined){
-                                                    complemnt = `c.CODIGO = ${codigo} AND
-                                                            c.ativo = 'S';`
-                                                }
-                                                const finalSql = sql + complemnt;
-                                                const [ resultVerifyClient  ] = await dbConn.query(finalSql);
+                                                 
+                                                
+                                                const params =[]
+                                                const values =[]
+
+                                             if(codigo && codigo != undefined){
+                                                   params.push(`c.CODIGO = ? `)
+                                                   values.push(codigo)
+                                              }
+
+                                             if(ativo && ativo != undefined){
+                                                   params.push(`c.ATIVO = ? `)
+                                                   values.push(ativo)
+                                              }
+
+                                              if(params.length > 0 ){
+                                                baseSql = baseSql+ ' WHERE '+ params.join(' AND ');
+                                              }
+                                                const finalSql = baseSql;
+                                                const [ resultVerifyClient  ] = await dbConn.query(finalSql,values);
                                                 return resultVerifyClient as cad_clie[];
 }
